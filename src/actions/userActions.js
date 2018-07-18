91a53c15b3a0160997c9ab63ba0c1e08c2a047df
Batman Bruce Wayne <taideptrai02@gmail.com>
2018-07-18 18:20:51 +0700

    update user list

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,6 +1,6 @@
 import { getUsersApi } from '../apis/userApis';
 import { httpSuccess, messageHttpRequest } from '../helpers/httpRequest';
-//import { formatPagination } from "../helpers/utility";
+import { formatPagination } from '../helpers/utility';
 
 export function getUsers(setState, params = {}) {
 	setState({ isLoading: true });
@@ -12,8 +12,8 @@ export function getUsers(setState, params = {}) {
 					data: res.data.data.users,
 					comments_count: res.data.data.comments_count,
 					posts_count: res.data.data.posts_count,
-					users_count: res.data.data.users_count
-					//pagination: formatPagination(res.data.meta)
+					users_count: res.data.data.users_count,
+					pagination: res.data.meta ? formatPagination(res.data.meta) : null
 				});
 			} else {
 				setState({ isLoading: false });
